c86d6d4ce57f9b9e33e554a878ce8a5c2be7cd6d
SeiyaIwabuchi <0123ook@gmail.com>
2022-11-20 19:24:40 +0900

    update

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
@@ -6,16 +7,33 @@ import {
   Route,
 } from "react-router-dom";
 import App from "./App";
+import Floor from "./Floor";
 import Readme from "./Readme";
 
+function NotFound() {
+  return (
+  <Floor>
+    <Box>
+      <Typography variant="h1">
+        404 NotFound.
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {window.location.pathname}
+      </Typography>
+    </Box>
+  </Floor>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />
   },
   {
-    path:"/readme",
+    path: "/readme",
     element: <Readme />
   }
 ]);
