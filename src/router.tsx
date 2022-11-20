90de00d6c8908c5d55df3318347117bbfaa86b13
SeiyaIwabuchi <0123ook@gmail.com>
2022-11-20 19:28:03 +0900

    update

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,38 @@
 
+import { Box, Typography } from "@mui/material";
 import {
   createBrowserRouter,
 } from "react-router-dom";
 import App from "./App";
+import Floor from "./Floor";
 import Readme from "./Readme";
 
+function NotFound() {
+  return (
+  <Floor>
+    <Box>
+      <Typography variant="h1">
+        404 NotFound.
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {window.location.pathname}
+      </Typography>
+    </Box>
+  </Floor>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />
   },
   {
     path:"/readme",
     element: <Readme />
   }
-],{basename:"seiyaiwabuchi.github.io/my-react-boilerplate/"});
+],{basename:"/seiyaiwabuchi.github.io/my-react-boilerplate/"});
 
 export default router;
